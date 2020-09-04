3b26a012def17127f0981fca1828f3d52f7c3c3a
Nicholas Wong <nicowong8@gmail.com>
2020-09-03 22:44:39 -0700

    accounted for changing State::isAccepting field to State::type; impl. run() public member function

--- TM.cpp
+++ TM.cpp
@@ -7,9 +7,9 @@ TM::TM() : startState(nullptr), acceptState(nullptr), rejectState(nullptr), curr
 {
 }
 
-State* TM::addState(std::string name, bool isAccepting)
+State* TM::addState(std::string name, StateType type)
 {
-    State* s = new State(name, isAccepting);
+    State* s = new State(name, type);
 
     currentState = s;
     return s;
@@ -26,4 +26,35 @@ void TM::addTransition(char readSym, char writeSym, Direction dir, State* nextSt
     {
         std::cout << "Error: Trying to add a transition but currentState == nullptr" << std::endl;
     }
+}
+
+// Run the TM on the input string and return the resulting tape output string
+std::string TM::run(std::string input)
+{
+    State *s = startState;
+    int i = 0;
+
+    while (!(s == acceptState || s == rejectState))
+    {
+        char c = input.at(i);
+
+        Transition *trans = s->getTransition(c);
+        input[i] = trans->getWriteSym();        // Write the symbol to the tape
+        trans->getDirection() == 0 ? i-- : i++; // Move the position i according to the transition function
+        s = trans->getNextState();              // Switch to the next state
+
+        if (i > input.size() - 1)
+        {
+            // if moving R past end of tape, add a blank symbol '_'
+            input.append("_");
+        }
+
+        if (i < 0)
+        {
+            // if moving L past beginning of tape, stay at leftmost position
+            i = 0;
+        }
+    }
+
+    return input;
 }
