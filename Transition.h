9731ebbeab0de79ac5223ecdf9e93b8394d776c4
Nicholas Wong <nicowong8@gmail.com>
2020-09-03 22:45:41 -0700

    nothing rly

--- Transition.h
+++ Transition.h
@@ -1,19 +1,23 @@
 #ifndef TRANSITION_H
 #define TRANSITION_H
 
-enum Direction {dirL, dirR};
+enum Direction
+{
+    dirL = 0,
+    dirR = 1
+};
 
 class Transition
 {
 public:
     // Parameterized Constructor
     Transition(char writeSym, Direction direction, State* nextState);
-    
+
     // Getter Member Functions
     char getWriteSym();
     Direction getDirection();
     State* getNextState();
-    
+
 private:
     char writeSym;
     Direction direction;
