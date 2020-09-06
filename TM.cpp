0a467581d80ffaf5c53d1293e29def77221f272e
Nico Wong <nicowong8@gmail.com>
2020-09-05 18:00:21 -0700

    Partially implemented accepts(): copied body of run() to accepts()

--- TM.cpp
+++ TM.cpp
@@ -76,3 +76,49 @@ std::string TM::run(std::string input)
 
     return input;
 }
+
+bool TM:accepts(std::string input)
+{
+    State* s = startState;
+    int i = 0; // index for tape head
+
+    if (s == nullptr)
+    {
+        // Check if start state is valid before dereferencing
+        std::cout << "Error: startState is a nullptr" << std::endl;
+        return input;
+    }
+
+    StateType type = s->getType();
+
+    while (!(type == acceptStateType || type == rejectStateType))
+    {
+        char c = input.at(i);
+
+        Transition *trans = s->getTransition(c);
+        input[i] = trans->getWriteSym();        // Write the symbol to the tape
+        trans->getDirection() == 0 ? i-- : i++; // Move the position i according to the transition function
+        s = trans->getNextState();              // Switch to the next state
+
+        if (s == nullptr)
+        {
+            // Check if current state is valid before deref.
+            std::cout << "Error: a transition leads to a nullptr as its nextState" << std::endl;
+            return input;
+        }
+
+        type = s->getType();                    // Update current state type
+
+        if (i > input.size() - 1)
+        {
+            // if moving R past end of tape, add a blank symbol '_'
+            input.append("_");
+        }
+
+        if (i < 0)
+        {
+            // if moving L past beginning of tape, stay at leftmost position
+            i = 0;
+        }
+    }
+}
