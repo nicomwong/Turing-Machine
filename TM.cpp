1de319cfae5f4afda96523f9962d7a3bcf0deec3
Nico Wong <nicowong8@gmail.com>
2020-09-05 17:50:32 -0700

    Added to run(): check if the State* is a nullptr before dereferencing; update current state type after each transition to make while loop condition work

--- TM.cpp
+++ TM.cpp
@@ -34,6 +34,13 @@ std::string TM::run(std::string input)
     State *s = startState;
     int i = 0;
 
+    if (s == nullptr)
+    {
+        // Check if start state is valid before dereferencing
+        std::cout << "Error: startState is a nullptr" << std::endl;
+        return input;
+    }
+
     StateType type = s->getType();
 
     while (!(type == acceptStateType || type == rejectStateType))
@@ -44,6 +51,15 @@ std::string TM::run(std::string input)
         input[i] = trans->getWriteSym();        // Write the symbol to the tape
         trans->getDirection() == 0 ? i-- : i++; // Move the position i according to the transition function
         s = trans->getNextState();              // Switch to the next state
+
+        if (s == nullptr)
+        {
+            // Check if current state is valid before deref.
+            std::cout << "Error: a transition leads to a nullptr as its nextState" << std::endl;
+            return input;
+        }
+
+        type = s->getType();                    // Update current state type
 
         if (i > input.size() - 1)
         {
