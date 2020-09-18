4f697d2ac945936f7fef2f211f7301c0fd1ac95e
Nicholas Wong <nicowong8@gmail.com>
2020-09-18 13:15:20 -0700

    Added another small comment

--- TM.h
+++ TM.h
@@ -14,6 +14,7 @@ public:
     bool addState(std::string stateName, StateType stateType); // Add the state to the machine; returns false iff state with name stateName existed before
     bool addTransition(std::string stateName, char readSym, char writeSym, Direction dir, std::string nextStateName);   // Add the transition to the state pointed to by currentState; returns true iff transition was added successfully
                                                                                                                         // This function requires that a state with name stateName exists
+                                                                                                                        // If no state with name nextStateName exists, it will be created (as a normal state type)
     /* In the future, the two above functions (or just addTransition())) will use exceptions instead of returning bool */
 
     std::string run(std::string input); // Runs the TM on the input and returns the resulting tape string
