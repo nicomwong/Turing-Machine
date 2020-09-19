972528d2022f064267fb9f7b2d2dfa2e2537a335
Nicholas Wong <nicowong8@gmail.com>
2020-09-18 18:44:20 -0700

    Implemented overloaded operator<< for enum Direction

--- Transition.h
+++ Transition.h
@@ -2,12 +2,20 @@
 #define TRANSITION_H
 
 #include <string>
+#include <ostream>
 
+// Maybe make Direction.h?
 enum Direction
 {
     dirL = 0,
     dirR = 1
 };
+
+std::ostream& operator<<(std::ostream& strm, Direction const& d)
+{
+    char dir = d == Direction::dirL ? 'L' : 'R';
+    return strm << dir;
+}
 
 class State; // forward declaration
 
@@ -22,8 +30,10 @@ public:
     std::string getNextState() const;
 
     /* Overloaded Operators */
-    bool operator==(Transition const& other);
+    bool operator==(Transition const& other) const;
 
+    friend std::ostream& operator<<(std::ostream& strm, Transition const& trans);
+    
 private:
     char writeSym;
     Direction direction;
