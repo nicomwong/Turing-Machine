b75adf83ce641886a6060389f9ce19f37a2aeee7
Nicholas Wong <nicowong8@gmail.com>
2020-10-03 17:50:07 -0700

    Added and implemented test for State getters

--- TMParserTest.cpp
+++ TMParserTest.cpp
@@ -5,23 +5,18 @@
 #include "TM_Parser.h"
 #include "tdd_funcs.h"
 
-void testTransition();
 void testTransitionGetters();
 void testTransitionOperatorEquals();
 
-void testState();
 void testStateGetters();
 
 int main()
 {
-    testTransition();
-    return 0;
-}
-
-void testTransition()
-{
     testTransitionGetters();
     testTransitionOperatorEquals();
+
+    testStateGetters();
+    return 0;
 }
 
 void testTransitionGetters()
@@ -31,10 +26,13 @@ void testTransitionGetters()
     Transition t('a', 'b', Direction::dirL, "q1");
     assertEquals(t.getRead(), 'a', "getRead()");
     assertTrue( !(t.getRead() == 'b'), "getRead()");
+
     assertEquals(t.getWrite(), 'b', "getWrite()");
     assertTrue( !(t.getWrite() == 'c'), "getWrite()");
+
     assertEquals(t.getDirection(), Direction::dirL, "getDirection()");
     assertTrue( !(t.getDirection() == Direction::dirR), "getDirection()");
+    
     assertEquals(t.getNextState(), "q1", "getNextState()");
     assertTrue( !(t.getNextState() == "q3"), "getNextState()");
 }
@@ -61,4 +59,33 @@ void testTransitionOperatorEquals()
 
     Transition t7('b', 'c', Direction::dirR, "q3");
     assertTrue( !(t1 == t7), "Different everything");
+}
+
+void testStateGetters()
+{
+    startTestGroup("Test State Getters");
+
+    State s("q0", StateType::startStateType);
+    
+    /* Test getName() */
+    assertEquals(s.getName(), "q0", "getName()");
+    assertTrue( !(s.getName() == "q1"), "getName()");
+
+    /* Test getType() */
+    assertEquals(s.getType(), StateType::startStateType, "getType()");
+    assertTrue( !(s.getType() == StateType::normalStateType), "getType()");
+
+    /* Test isAccepting() and isRejecting() */
+    assertTrue( !s.isAccepting(), "isAccepting()");
+    assertTrue( !s.isRejecting(), "isRejecting()");
+
+    State s_acc("acc", StateType::acceptStateType);
+    assertTrue(s_acc.isAccepting(), "isAccepting()");
+    
+    State s_rej("rej", StateType::rejectStateType);
+    assertTrue(s_rej.isRejecting(), "isRejecting()");
+
+    /* Test getTransition() */
+
+    assertEquals<Transition>(s.getTransition('a'), nullptr, "No transitions");    
 }
